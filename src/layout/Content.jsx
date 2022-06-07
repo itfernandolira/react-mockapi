a77083a92c37e6a8c15461abe7fdba116ec21bdb
Fernando Lira <it.fernandolira@gmail.com>
2022-06-07 10:44:23 +0100

    Project init

---
+++ src/layout/Content.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import Movies from "./Movies";
+import Movie from "./Movie";
+import NotFound from "../components/NotFound";
+
+const Content = props => {
+    return (
+        <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/:category" element={<Movies />} />
+            <Route path="/:category/movies/:movie" element={<Movie />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
+}
+
+export default Content;
---
+++ src/layout/Posts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Post from "../components/Post";
+import AddPost from "./AddPost";
+
+
+const Posts = props => {
+    return (
+        <>
+        <Post></Post>
+        <AddPost></AddPost>
+        </>
+    );
+}
+
+export default Posts;
